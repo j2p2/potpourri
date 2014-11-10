be4f3a575f7bb0b4b667b27370ee505d71900ec5
j2pierce <j2pierce@gmail.com>
2014-11-10 17:40:37 -0600

    added cmd line adders to factorial

--- clisp/factorial.lisp
+++ clisp/factorial.lisp
@@ -1,2 +1,19 @@
-(defun factorial (x) (if (< x 2) x (* x (factorial (- x 1)))))
+(defun factorial (x) 
+	(if (< x 2) 
+	x (* x (factorial (- x 1)))
+	)
+)
+
+(defun adder (x y)
+	(+ x y)
+)
+
 (princ (factorial (parse-integer (first *args*))))
+
+(print (adder 	(parse-integer (first *args*)) 
+		(if (second *args*) 
+			(parse-integer (second *args*)) 
+			10
+		)
+	)
+)
